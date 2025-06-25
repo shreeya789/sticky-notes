883ff9837f32529cea1b8ed4518ae14f624f0ed3
shreeya789 <shreeyabhttarai974.com>
2025-06-25 18:41:56 +0545

    added localstorage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NoteCard from "./NoteCard";
 
-const App = () => {
-  const [data, setData] = useState([]);
+const App = () => {;
+  const[data,setData]= useState(JSON.parse(localStorage.getItem("notes"))||[])
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("");
+
+  useEffect(()=>{
+    localStorage.setItem("notes", JSON.stringify(data));
+  },[data])
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setData([
       ...data,
       { title: title, description: description, priority: priority },
     ]);
+
     setTitle("");
     setDescription("");
     setPriority("");
   };
+
   function handleTitle(e) {
     setTitle(e.target.value);
   }
@@ -25,37 +32,42 @@ const App = () => {
   function handlePriority(e) {
     setPriority(e.target.value);
   }
-  function handleRemove(i){
-    let value = window.confirm("Are you sure you want to delete this note")
-    if (value){
-    setData(data.filter((item,index)=>i!==index))
+
+  function handleRemove(i) {
+    
+    let value = window.confirm("Are you sure you want to delete this note?");
+    if(value){
+      let newData = data.filter((_, index) => i !== index)
+       setData(newData); 
+       localStorage.setItem("notes", JSON.stringify(newData));
     }
     else{
-      return
+        return
     }
   }
+
   return (
     <div className="p-8 flex flex-col gap-8 w-screen">
       <h1 className="text-3xl">Sticky Notes</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6">
         <input
           type="text"
           placeholder="Enter title"
           value={title}
           onChange={handleTitle}
-          className="w-[400px] h-fit p-2 border-2 border-blue-400"
+          className="w-[400px] h-fit p-2 text-lg border-2 border-blue-400 rounded-md"
         />
-        <input
+        <textarea
           type="text"
-          placeholder="Enter Description "
+          placeholder="Enter Description"
           value={description}
           onChange={handleDescription}
-          className="w-[400px] h-fit p-2 border-2 border-blue-400"
+          className="w-[400px] h-fit p-2 text-lg border-2 border-blue-400 rounded-md"
         />
         <select
           value={priority}
           onChange={handlePriority}
-          className="w-[180px] h-fit p-2 text-lg border-2 border-blue-400"
+          className="w-[180px] h-fit p-2 text-lg border-2 border-blue-400 rounded-md"
         >
           <option value="">Select Priority</option>
           <option value="High">High</option>
@@ -64,19 +76,20 @@ const App = () => {
         </select>
         <button
           type="submit"
-          className="bg-blue-500 px-6 py-6 text-white w-fit h-fit text-xl font-semibold cursor-pointer rounder-lg"
+          className="bg-blue-500 px-6 py-4 text-white w-fit h-fit text-xl font-semibold cursor-pointer rounded-lg"
         >
           Add
         </button>
       </form>
-      <div className="grid grid-cols-4 gap-48 !w-full">
+
+      <div className="grid grid-cols-4 gap-6">
         {data.map((item, index) => {
           return (
             <NoteCard
               key={index}
               title={item.title}
               description={item.description}
-              priority={item.priority} 
+              priority={item.priority}
               remove={()=>{handleRemove(index)}}
             />
           );
